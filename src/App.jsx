d21e763aae1511fedd2467e3576822c2a37a87dc
Kush2205 <kr.kushagra2205@gmail.com>
2024-09-03 22:42:06 +0530

    routing added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Layout from './Layout';
+
+import Contact from './components/Contact';
+import Home from './components/Home';
+import Events from './components/Events'
+
+
+
 function App() {
 
+ 
 
   return (
-    <>
-      
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home/>}/>
+            <Route path="contact" element={<Contact />} />
+            <Route path="Events" element={<Events />} />
+           
+          
+        </Route>
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
