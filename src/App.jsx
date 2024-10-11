ec358f7346197c746d4d30e15438c8df4d27fb92
Kush2205 <kr.kushagra2205@gmail.com>
2024-10-11 10:57:11 +0530

    Events Page modified

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,12 @@ import About from './components/About';
 import Gallery from './components/Gallery';
 import Ambassador from './components/Ambassador';
 import Sponsors from './components/Sponsors';
-import EventCategory from './components/EventCategory';
-
+import EventCategory1 from './components/EventCategory1';
+import EventCategory2 from './components/EventCategory2';
+import EventCategory3 from './components/EventCategory3';
+import EventCategory4 from './components/EventCategory4';
+import EventCategory5 from './components/EventCategory5';
+import EventCategory6 from './components/EventCategory6';
 function App() {
 
  
@@ -21,8 +25,13 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home/>}/>
             <Route path="contact" element={<Contact />} />
-            <Route path="event" element={<Events />} />
-            <Route path="/event/:eventId" element={<EventCategory />} />
+            <Route path="Events" element={<Events />} />
+            <Route path="/events-category-1" element={<EventCategory1 />} />
+            <Route path='/events-category-2' element={<EventCategory2/>} />
+            <Route path = '/events-category-3' element={<EventCategory3/>} />
+            <Route path = '/events-category-4' element={<EventCategory4/>} />
+            <Route path = '/events-category-5' element={<EventCategory5/>} />
+            <Route path = '/events-category-6' element={<EventCategory6/>} />
             <Route path="About" element={<About />} />
             <Route path="Gallery" element={<Gallery />} />
             <Route path="Ambassadors" element={<Ambassador/>} />
