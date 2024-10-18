57f992588d9ee354e194efb8421390153fb39fdf
Shubh <shubhsinghrajpoot6665@gmail.com>
2024-10-18 19:50:12 +0530

    Sponsorship Added & Cards Aligned

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import background from "../assets/Images/newbg.jpg";
-import rgipt from "../assets/Images/rgiptp.jpg"
-import { GridBackgroundDemo } from './ui/Background/GridBackgroundemo';
+import rgipt from "../assets/Images/rgiptp.jpg";
 
 export default function About() {
 
 
     return (
         <>
-        <GridBackgroundDemo />
+        
         </>
     )
 }
--- src/components/sponsors.jsx
+++ src/components/sponsors.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
+import * as THREE from "three"
+import Cyl from "../Cyl";
+import { Bloom, EffectComposer, ToneMapping } from '@react-three/postprocessing'
+
 
 export default function Sponsors(props) {
-    
-
     return (
         <>
-            
+        <div className='flex flex-col h-[100vh] bg-black bg-opacity-30'>
+            <h3 className="font-Space text-center font-semibold text-3xl md:mb-[-6%] lg:mb-[3] md:text-4xl lg:text-5xl text-white">
+                     OUR SPONSORS
+            </h3>
+            <div className='mb-14 h-full'>
+                <Canvas camera={{ fov: 35, position: [0, 0, 10] }} className='h-full'>
+                    <ambientLight />
+                    <Cyl />
+                    <EffectComposer>
+                        <Bloom intensity={0.8} luminanceThreshold={0} luminanceSmoothing={0} />
+                        <ToneMapping adaptive />
+                    </EffectComposer>
+                </Canvas>
+            </div>  
+        </div>
         </>
     )
 }
